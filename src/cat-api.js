9e1bd7da80828b1ee11d342d8514bd39a5e7d60e
SweetLanaMay <121062437+SweetLanaMay@users.noreply.github.com>
2023-06-08 21:32:51 +0300

    not finished hw-10

--- src/cat-api.js
+++ src/cat-api.js
@@ -12,7 +12,7 @@ export function fetchBreeds() {
 }
 
 export function fetchCatByBreed(breedId) {
-  const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
+  const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=live_M9HZxBw55O4pnuOdTXAauSUY49IdT6kF4yZqQatXYnEOJMgzu0cNNFYk5VnQW1UJ`;
 
   return fetch(url)
   .then(responce => {
--- src/index.js
+++ src/index.js
@@ -8,28 +8,31 @@ const catInfo = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader-text');
 const error = document.querySelector('.error');
 
-initPage()
+initPage();
 function initPage() {
-  fetchBreeds().then(data => {
-    renderOptions(data)
-  })
-  .catch((error) => console.log(error),
-  Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!'))
+  fetchBreeds()
+    .then(data => {
+      renderOptions(data);
+    })
+    .catch(
+      error => console.log(error),
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      )
+    );
 }
 
-
 function renderOptions(data) {
-    const markupBreeds = data
-      .map(({ id, name }) => {
-        return `<li><option value ='${id}'>${name}</option></li>`;
-      })
-      .join('');
-    selectElement.insertAdjacentHTML('beforeend', markupBreeds);
-    new SlimSelect({
-            select: '#single'
-        });
-  }
-
+  const markupBreeds = data
+    .map(({ id, name }) => {
+      return `<li><option value ='${id}'>${name}</option></li>`;
+    })
+    .join('');
+  selectElement.insertAdjacentHTML('beforeend', markupBreeds);
+  new SlimSelect({
+    select: '#single',
+  });
+}
 
 selectElement.addEventListener('change', () => {
   const selectedBreedId = selectElement.value;
@@ -39,9 +42,11 @@ selectElement.addEventListener('change', () => {
 
   fetchCatByBreed(selectedBreedId)
     .then(data => renderSelectedBreed(data))
-    .catch((error) => 
-    console.log(error),
-    Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!')
+    .catch(
+      error => console.log(error),
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      )
     )
     .finally(() => {
       loader.classList.add('hidden');
@@ -49,63 +54,14 @@ selectElement.addEventListener('change', () => {
     });
 });
 
-function renderSelectedBreed(data) {
-  const markupSelectedBreed = data[0]
-    .map(({ image, name, description, temperament }) => {
-
-      return `<img class="cat_image" 
-  src ="${image.url}"
-  alt="${name}"/>
-  <h2 class="cat_breed_title">${name}</h2>
-  <p class="cat_description">${description}</p>
-  <p class="cat_temperament">Temperament: ${temperament}</p>
+const renderSelectedBreed = breed => {
+  const markupSelectedBreed = `<img class="cat_image" 
+  src ="${breed.url}"
+  alt="${breed.breeds[0].name}"/>
+  <h2 class="cat_breed_title">${breed.breeds[0].name}</h2>
+  <p class="cat_description">${breed.breeds[0].description}</p>
+  <p class="cat_temperament">Temperament: ${breed.breeds[0].temperament}</p>
   `;
-    })
-    .join('');
 
-  catInfo.innerHTML = markupSelectedBreed;
-}
-
-function showLoader() {
-  loader.classList.remove('hidden');
-}
-
-function hideLoader() {
-  loader.classList.add('hidden');
-}
-
-function showError() {
-  error.classList.remove('hidden');
-}
-
-function hideError() {
-  error.classList.add('hidden');
-}
-
-  selectElement.addEventListener('change', () => {
-    const selectedBreedId = selectElement.value;
-  
-    showLoader();
-    hideError();
-    fetchCatByBreed(selectedBreedId)
-      .then(renderSelectedBreed)
-      .catch(() => {
-        showError();
-      })
-      .finally(() => {
-        hideLoader();
-      });
-  });
-
-  document.addEventListener('DOMContentLoaded', () => {
-    showLoader();
-    hideError();
-    fetchBreeds()
-      .then(renderOptions)
-      .catch(() => {
-        showError();
-      })
-      .finally(() => {
-        hideLoader();
-      });
-  });
+  catInfo.innerHTML(markupSelectedBreed);
+};
